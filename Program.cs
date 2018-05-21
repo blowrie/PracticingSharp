bcfe53b5947f11c3c8ea037bbe71cdec315a683e
blowrie <blowrie8@gmail.com>
2018-05-21 18:08:05 -0400

    First Commit
    Committing practice problems in C#

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Untitled
+{
+    static void Main(string[] args)
+    {
+        // Problem 1
+        //Write a program in C# Sharp to read 10 numbers from keyboard and find their sum and average.
+
+        /*
+         int[] number = new int[10];
+        int sum = 0;
+        int average = 0;
+
+        Console.WriteLine("Enter 10 numbers");
+        for (int i = 0; i < number.Length; i++)
+        {
+            number[i] = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i < number.Length; i++)
+        {
+            sum = sum + number[i];
+        }
+
+        average = sum / 10;
+        Console.WriteLine("The sum of the ten numbers you entered is: " + sum);
+        Console.WriteLine("The average of the ten numbers you entered is: " + average);
+
+        //Problem Two
+        Write a program in C# Sharp to display the cube of the number upto given an integer.
+
+        int number = 0;
+        int cube = 0;
+
+        Console.WriteLine("Enter the cube of the max number you want");
+        number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= number; i++) {
+            cube = i * i;
+            Console.WriteLine("The number is: " + i + " and the cube of " + i + " is " + cube); 
+        }
+
+
+        //Problem 3
+        //Write a program in C# Sharp to display the multiplication table of a given integer.
+
+        int number = 0;
+        int sum = 0;
+
+        Console.WriteLine("What is the number that you would like a mutiplication table for");
+        number = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++) {
+            sum = i * number;
+            Console.WriteLine(number + " X " + i + " = " + sum);
+        }
+        */
+
+
+//End Brackets
+    }
+}
